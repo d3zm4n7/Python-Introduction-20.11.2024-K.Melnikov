b37294d6d6b58fcd5d5025fdab0a1c0fa06beb8a
opilane <opilane@HP-CZC2349HTB>
2024-11-20 15:47:26 +0200

    Ülesanne 1 oli tehtud

--- Python_Introduction_20.11.2024_K.Melnikov.py
+++ Python_Introduction_20.11.2024_K.Melnikov.py
@@ -1,1 +1,8 @@
+#Ülesanne 1
 print("Hello World!")
+nimi=input("What's your name?")
+print("Hello World! Greeetings! How's your day", nimi, "?")
+print("Hello World! Greeetings! How's your day "+ nimi, "?")
+vanus=int(input("How old are you? "))
+print("Hello World! Greetings! "+nimi+" You're",vanus, "years old!")
+print(f"\tHello World! \nGreetings! {nimi} You're {vanus} years old!") #f даёт возможность использовать фигурные скобки, \t Красная строка \n Новая строка
