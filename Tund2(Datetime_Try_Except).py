05ec9a1a7e4b8d1e97d3241d7f8510448e0ab53e
Kirill Melnikov <Kirill.Melnikov@tthkee.onmicrosoft.com>
2024-11-28 15:45:39 +0200

    tehtud kodutöö

--- Tund2(Datetime_Try_Except).py
+++ Tund2(Datetime_Try_Except).py
@@ -67,4 +67,50 @@ from re import A
 # print(f"On vaja {kogus} mündi!\nMeil on vaja {kogus*2} eur")
 
 #Ülesanne 5
-sõna1 = "Kill-koll"
+a1 = "kill-koll ".capitalize()
+a2 = "killadi-koll ".capitalize()
+print(f"{a1}{a1}{a2}{a1}{a1}{a2}{a1}{a1}{a1}{a1}")
+# print(f"""kill-koll kill-koll killadi-koll kill-koll kill-koll killadi-koll kill-koll kill-koll kill-koll
+# kill-koll""") #testimiseks panin print("""text""") selleks et see lause võttab 2 rida
+
+#Ülesanne 6
+y1 = "Rong see sõitis tsuhh tsuhh tsuhh,".upper()
+y2 = "piilupart oli rongijuht.".upper()
+y3 = "Rattad tegid rat tat taa,".upper()
+y4 = "rat tat taa ja tat tat taa.".upper()
+y5 = "Aga seal rongi peal,".upper()
+y6 = "kas sa tead, kes olid seal?".upper()
+
+y7 = "Rong see sõitis tuut tuut tuut,".upper()
+y8 = "piilupart oli rongijuht.".upper()
+y9 = "Rattad tegid kill koll koll,".upper()
+y10 = "kill koll koll ja kill koll kill.".upper()
+y11 = "....Sisend, väljund, tingimus....".upper()
+print(f"{y1}\n{y2}\n{y3}\n{y4}\n{y5}\n{y6}\n\n{y7}\n{y8}\n{y9}\n{y10}\n\n\n\n{y11}")
+
+#Ülesanne7
+a = float(input("Palun sisestage esimene ristküliku külg: "))
+b = float(input("Palun sisestage teine ristküliku külg: "))
+Pr = 2 * (a + b)
+Sr = a * b
+print(f"Ristküliku ümbermõõt on {Pr} ja pindala on {Sr}!")
+
+#Ülesanne 8
+litres = float(input("Palun sisestage tangitud kütuse liitrid: "))
+kms = float(input("Palun sisestage läbitud kilomeetrid: "))
+kytusekulu = (litres / kms) * 100
+print(f"Teie keskmine kütusekulu on {round(kytusekulu,2)}!")
+
+#Ülesanne 9 - Rulluisutajad    Rulluisutaja keskmine kiirus on 29,9km/h    Kui kaugele jõuab M minutiga
+minutid = float(input("Sisesta aeg minutites: "))
+kiirus_kmh = 29.9
+tunnid = minutid / 60
+kaugus = kiirus_kmh * tunnid
+print(f"Rulluisutaja jõuab {minutid} minutiga {round(kaugus,2)} kaugusele")
+
+#Ülesanne 10 
+
+minutid1 = int(input("Sisesta aeg minutites: "))
+tunnid1 = minutid1 // 60
+alles_minutid = minutid1 % 60
+print(f"{tunnid1}:{alles_minutid}")
