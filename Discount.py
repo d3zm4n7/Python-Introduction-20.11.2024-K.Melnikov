16c2ee57e0cb0e5d70cae1fe5adadc731ec92155
Gleb Sotsjov <glebsotjov@gmail.com>
2024-12-18 16:14:06 +0200

    discount, а вообще схему со светофорами

---
+++ Discount.py
@@ -0,0 +1,9 @@
+from time import *
+print("Welcome to DISCOUNT APP!")
+
+print("You have a good chance to win a discount up to 20% off!")
+print("1 - Сложение и вычитание")
+
+
+a = int(input("Sisesta summa:")))
+sleep(1) # 1 sek
