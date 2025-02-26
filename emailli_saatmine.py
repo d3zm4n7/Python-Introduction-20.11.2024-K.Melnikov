7d808630145398193c0bebb12f8e9518c45a6764
opilane <opilane@HP-CZC2349HTZ>
2025-02-26 12:22:30 +0200

    Email file added

---
+++ emailli_saatmine.py
@@ -0,0 +1,15 @@
+﻿import tkinter as tk
+
+
+# Окно
+window = tk()
+window.geometry("550x200")
+window.resizable(False,False) #x=False y=False disables resize of the programm window
+window.title("Квадратные уравнения")
+window.configure(bg="beige")
+
+
+lbl_email = tk.label(window, text="E-mail:")
+lbl_sub = tk.label(window, text="Subject:")
+lbl_attach = tk.label(window, text="Attach File:")
+
